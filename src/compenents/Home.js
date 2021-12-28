015bd7d45c75bc6350ba16b106dd3170fb913e31
Bloo00 <kabin.yangg@gmail.com>
2021-12-27 20:01:56 -0800

    made home route

---
+++ src/compenents/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import './Home';
+
+class Home extends Component {
+    render () {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default Home;
